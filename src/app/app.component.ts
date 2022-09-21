ad9ddf3f7ca53cd4407a72ec33f36943674e18b9
perumalps02 <perumal.ps@finstein.ai>
2022-09-21 15:35:45 +0530

    using inputButton and checkbox for one task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,17 @@ export class AppComponent {
   title = 'first';
   value = 'values';
   printEvent='';
+  taskList:Array<any>=[];
   print (event:string){
     console.log(event);
     this.printEvent = event;
     
+  }
+  addTasks(task:string){
+    this.taskList.push({taskName:task,isChecked:false})
+    console.log(this.taskList);
+  }
+  saveToBackend(){
+    console.log(this.taskList);   
   }
 }
