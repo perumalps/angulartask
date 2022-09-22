2d7208297decea4214db49183d17845f741f1ab9
perumalps02 <perumal.ps@finstein.ai>
2022-09-22 12:18:22 +0530

    practising the routing againing once

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 import { ContectusComponent } from './contectus/contectus.component';
 
 const routes: Routes = [
   {
     path:'aboutus',
     component:AboutusComponent,
-    children:[{
-      path:"component1",
-      component:Component1Component
-    }]
+    children : [{
+      path : 'component1',
+      component : Component1Component
+    }] 
   },
   {
-    path:'aboutus/component1',
-    component:Component1Component
-  },{
-    path:'contectus',
-    component:ContectusComponent
-  }
+    path : 'component2',
+    component : Component2Component
+  },
+  
 ];
 
 @NgModule({
